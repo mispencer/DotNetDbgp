20f4fdf8e0f66710a9320b802d400c68c6d89b8b
Spencer G. Jones <spencer@egovmt.com>
2014-04-24 15:50:03 -0600

    Use nuget package for Mdbg

--- Code/DotNetDbgp.ClientDebugger/Client.cs
+++ Code/DotNetDbgp.ClientDebugger/Client.cs
@@ -22,11 +22,9 @@ namespace DotNetDbgp.ClientDebugger {
 
 		public void Start() {
 			var ip = IPAddress.Loopback;
-			//var ip = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
 			var ipEndPoint = new IPEndPoint(ip, 9000);
 
 			_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-			//_socket.DualMode = true;
 			_socket.Connect(ipEndPoint);
 
 			//new Thread(() => {
@@ -40,7 +38,7 @@ namespace DotNetDbgp.ClientDebugger {
 				var messageBuffer = "";
 
 				var engine = new MDbgEngine();
-				_mdbgProcess = engine.Attach(_pid, MdbgVersionPolicy.GetDefaultAttachVersion(_pid));
+				_mdbgProcess = engine.Attach(_pid, VersionPolicy.GetDefaultAttachVersion(_pid));
 				_mdbgProcess.AsyncStop().WaitOne();
 				
 				var sourcePosition = !_mdbgProcess.Threads.HaveActive ? null : _mdbgProcess.Threads.Active.CurrentSourcePosition;
@@ -48,6 +46,7 @@ namespace DotNetDbgp.ClientDebugger {
 				_socket.Send(Encoding.UTF8.GetBytes(this.GenerateOutputMessage(this.InitXml(sourcePosition != null ? sourcePosition.Path : null))));
 
 				Console.CancelKeyPress += delegate {
+					Console.Write("Exiting...");
 					this.Detach();
 					System.Environment.Exit(-1);
 				};
@@ -64,7 +63,7 @@ namespace DotNetDbgp.ClientDebugger {
 					while(messageBuffer.Contains("\0")) {
 						var message = messageBuffer.Substring(0, messageBuffer.IndexOf('\0'));
 #pragma warning disable 162
-						if (SHOW_MESSAGES) { Console.WriteLine("Message: "+message); }
+						if (SHOW_MESSAGES) { Console.WriteLine("Message: "+(message.Length > 1000 ? message.Substring(0, 1000) : message)); }
 #pragma warning restore 162
 
 						messageBuffer = messageBuffer.Substring(message.Length+1);
@@ -106,13 +105,8 @@ namespace DotNetDbgp.ClientDebugger {
 							case "step_into":
 							case "step_over":
 							case "step_out":
-								//if (_mdbgProcess.Threads.HaveActive) {
+								if (!_mdbgProcess.IsRunning) {
 									WaitHandle wait = null;
-									while(!(_mdbgProcess.StopReason is StepCompleteStopReason || _mdbgProcess.StopReason is BreakpointHitStopReason)) {
-										wait = _mdbgProcess.Go();
-										Console.WriteLine("Continuing - invalid stop");
-										wait.WaitOne();
-									}
 									switch (command) {
 										case "run":
 											wait = _mdbgProcess.Go();
@@ -126,9 +120,17 @@ namespace DotNetDbgp.ClientDebugger {
 										case "step_out":
 											wait = _mdbgProcess.StepOut();
 											break;
+										default:
+											throw new Exception("Assertion failed");
+									}
+									if (_mdbgProcess.StopReason is StepCompleteStopReason || _mdbgProcess.StopReason is BreakpointHitStopReason) {
+										Console.WriteLine(String.Format("Continuing - invalid stop: {0}", _mdbgProcess.StopReason));
+										wait = _mdbgProcess.Go();
+										wait.WaitOne();
+										Console.WriteLine("Continued from invalid stop");
 									}
 									wait.WaitOne();
-								//}
+								}
 								outputMessage = this.ContinuationXml(parsedMessage.Item1, transId);
 								break;
 							case "stop":
@@ -161,7 +163,6 @@ namespace DotNetDbgp.ClientDebugger {
 						_socket.Send(Encoding.UTF8.GetBytes(realMessage));
 					}
 				}
-				this.Detach();
 			} catch (Exception) {
 				try {
 					this.Detach();
@@ -176,7 +177,7 @@ namespace DotNetDbgp.ClientDebugger {
 			var length = message.Length;
 			var result = String.Format("{0}\0{1}\0", length.ToString(), message);
 #pragma warning disable 162
-			if (SHOW_MESSAGES) { Console.WriteLine(result); }
+			if (SHOW_MESSAGES) { Console.WriteLine("Response: "+(result.Length > 1000 ? result.Substring(0, 1000) : result)); }
 #pragma warning restore 162
 			return result;
 		}
@@ -227,7 +228,7 @@ namespace DotNetDbgp.ClientDebugger {
 			var framesString = String.Empty;
 			var currentDepth = depth == null ? 0 : depth.Value;
 			var sourcePositions = activeThread != null ? ( /* depth == null ? */ activeThread.Frames.Cast<MDbgFrame>() /* : activeThread.Frames.Cast<MDbgFrame>().Skip(depth.Value-1).Take(1) */).Select(i => i.SourcePosition)
-			                    : new MDbgSourcePosition[] { null };
+			                    : new SourcePosition[] { null };
 			foreach(var sourcePosition in sourcePositions) {
 				framesString += String.Format(
 					"<stack level=\"{0}\" type=\"file\" filename=\"{1}\" lineno=\"{2}\" where=\"\" cmdbegin=\"\" cmdend=\"\"/>",
@@ -254,8 +255,9 @@ namespace DotNetDbgp.ClientDebugger {
 					file = file.Substring(7);
 				}
 				file = file.Replace('/', '\\');
-				var breakpoint = _mdbgProcess.Breakpoints.CreateBreakpoint(new BreakpointLineNumberLocation(file, line));
-				Console.WriteLine(String.Format("\nFile: {0}, Line: {1}, Breakpoint: {2}\n", file, line, breakpoint.ToString()));
+				Console.WriteLine(String.Format("File: {0}, Line: {1}", file, line));
+				var breakpoint = _mdbgProcess.Breakpoints.CreateBreakpoint(file, line, true);
+				Console.WriteLine(String.Format("Breakpoint: {0}", breakpoint.ToString()));
 				return String.Format(
 					 "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 					+"<response xmlns=\"urn:debugger_protocol_v1\" command=\"breakpoint_set\" transaction_id=\"{0}\" state=\"{1}\" id=\"{2}\"/>",
@@ -267,9 +269,11 @@ namespace DotNetDbgp.ClientDebugger {
 		}
 
 		private String BreakpointRemoveXml(String transId, int id) {
-				var breakpoint = _mdbgProcess.Breakpoints.Cast<MDbgBreakpoint>().FirstOrDefault(i => i.Number == id);
+				var breakpoint = _mdbgProcess.Breakpoints.UserBreakpoints.FirstOrDefault(i => i.Number == id);
 				if (breakpoint != null) {
 					breakpoint.Delete();
+				} else {
+					Console.WriteLine("Breakpoint not found");
 				}
 				//Console.WriteLine(String.Format("File: {0}, Line: {1}, Breakpoint: {2}", file, line, breakpoint.ToString()));
 				return String.Format(
@@ -280,14 +284,15 @@ namespace DotNetDbgp.ClientDebugger {
 		}
 
 		private String ContextGetXml(String transId, int contextId, int depth) {
-			var frame = depth == 0 ? _mdbgProcess.Threads.Active.CurrentFrame : _mdbgProcess.Threads.Active.Frames.Cast<MDbgFrame>().ElementAt(depth);
-
 			var variables = new List<MDbgValue>();
-			if (contextId == 0 || contextId == 1) {
-				variables.AddRange(frame.Function.GetActiveLocalVars(frame));
-			}
-			if (contextId == 0 || contextId == 2) {
-				variables.AddRange(frame.Function.GetArguments(frame));
+			if (_mdbgProcess.Threads.HaveActive) {
+				var frame = depth == 0 ? _mdbgProcess.Threads.Active.CurrentFrame : _mdbgProcess.Threads.Active.Frames.ElementAt(depth);
+				if (contextId == 0 || contextId == 1) {
+					variables.AddRange(frame.GetActiveLocalVariables());
+				}
+				if (contextId == 0 || contextId == 2) {
+					variables.AddRange(frame.GetArguments());
+				}
 			}
 
 			var variablesString = new StringBuilder();
@@ -324,31 +329,32 @@ namespace DotNetDbgp.ClientDebugger {
 			);
 		}
 
-		private String ContextGetPropertyXml(MDbgValue var, int depth, string fullName = null) {
+		private String ContextGetPropertyXml(MDbgValue val, int depth, string fullName = null) {
 			if (fullName == null) {
-				fullName = var.Name;
+				fullName = val.Name;
 			}
 			var childPropertiesCount = 0;
 			var childPropertiesString = new StringBuilder();
 			if (depth > 0) {
-				if (var.IsArrayType) {
-					foreach(var child in var.GetArrayItems()) {
+				var managedValue = val as ManagedValue;
+				if (managedValue.IsArrayType) {
+					foreach(var child in managedValue.GetArrayItems().ToList()) {
 						childPropertiesString.Append(this.ContextGetPropertyXml(child, depth-1, fullName+"["+child.Name+"]"));
 						childPropertiesCount++;
 					}
 				}
-				if (var.IsComplexType) {
-					foreach(var child in var.GetFields()) {
+				if (managedValue.IsComplexType) {
+					foreach(var child in managedValue.GetFields()) {
 						childPropertiesString.Append(this.ContextGetPropertyXml(child, depth-1, fullName+"."+child.Name));
 						childPropertiesCount++;
 					}
 				}
 			}
 			Func<String,String> e = (String i) => this.EscapeXml(i);
-			var myValue = e(var.GetStringValue(0, false));
+			var myValue = e(val.GetStringValue(0, false));
 			return String.Format(
 				"<property name=\"{0}\" fullname=\"{1}\" type=\"{2}\" classname=\"{2}\" constant=\"0\" children=\"{3}\" size=\"{4}\" encoding=\"none\" numchildren=\"{3}\">{5}{6}</property>",
-				e(var.Name), e(fullName), e(var.TypeName), childPropertiesCount, myValue.Length+childPropertiesString.Length, myValue, childPropertiesString.ToString()
+				e(val.Name), e(fullName), e(val.TypeName), childPropertiesCount, myValue.Length+childPropertiesString.Length, myValue, childPropertiesString.ToString()
 			);
 		}
 
